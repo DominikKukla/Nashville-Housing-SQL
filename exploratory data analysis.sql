cf0ad36e81a982e893ad5c256d22cc588bffc6f3
Dominik Kukla <dominik.kukla@onet.pl>
2024-07-06 22:42:39 +0200

    Grammar fixes

--- exploratory data analysis.sql
+++ exploratory data analysis.sql
@@ -13,8 +13,8 @@ SELECT PropertyCity,
 FROM nashville
 GROUP BY PropertyCity
 ORDER BY 2 DESC;
-# Majority of properties are in Nashville (71% of all listed)
-# Franklin and Bellevue both have only one property listed
+# The majority of properties are in Nashville (71% of all listed).
+# Franklin and Bellevue both have only one property listed.
 
 -- Average property sale price between cities
 SELECT PropertyCity,
@@ -23,10 +23,10 @@ SELECT PropertyCity,
 FROM nashville
 GROUP BY PropertyCity
 ORDER BY 2 DESC;
-# Average property sale price is highest in Nashville (366615), which is the only
-# city with average sale price exceeding overall average (327512).
+# The average property sale price is highest in Nashville (366615), which is the only
+# city with an average sale price exceeding the overall average (327512).
 # Bellevue is an outlier due to having only one property listed and
-# it being more than tenfold cheaper (25000) compared to overall average (327512).
+# being more than tenfold cheaper (25000) compared to the overall average (327512).
 
 -- Average property year of build between cities
 SELECT PropertyCity,
@@ -35,8 +35,8 @@ FROM nashville
 WHERE YearBuilt IS NOT NULL
 GROUP BY PropertyCity
 ORDER BY 2 DESC;
-# The averagely newest properties are located in Antioch (1983)
-# while averagely oldest properties are located in Old Hickory (1958).
+# The averagely newest properties are located in Antioch (1983),
+# while the averagely oldest properties are located in Old Hickory (1958).
 
 -- Correlation between YearBuilt and SalePrice
 SELECT YearBuilt,
@@ -63,10 +63,10 @@ SELECT YearBuilt,
 	END AS Status
 FROM YearBuiltCTE;
 # Data before 1899 is inconsistent and won't be looked at.
-# Comparison between YearBuilt and average SalePrice shows no signs of linear correlation.
-# Properties built between 1908 and 1916 on average are sold pricier that overall average.
-# Properties built between 1942 and 1992 on average are sold cheaper that overall average.
-# Properties built between 2002 and onwards on average are sold cheaper that overall average.
+# THe comparison between YearBuilt and average SalePrice shows no signs of linear correlation.
+# Properties built between 1908 and 1916, on average, are sold at a higher price than the overall average.
+# Properties built between 1942 and 1992, on average, are sold cheaper that overall average.
+# Properties built between 2002 and onwards, on average, are sold cheaper that overall average.
 
 -- Count of properties per LandUse
 SELECT LandUse,
@@ -76,5 +76,5 @@ FROM nashville
 GROUP BY LandUse
 ORDER BY 2 DESC;
 # The vast majority of properties are intended for residential use.
-# The broadest usage is for single family housing (60.5%),
-# then for residential condominiums (25.4%).
+# The broadest usage is for single-family housing (60.5%),
+# followed by for residential condominiums (25.4%).
